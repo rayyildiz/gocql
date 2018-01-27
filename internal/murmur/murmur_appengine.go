2b303d36948846bfe55b9e3e888df3167b4a04ed
Ramazan AYYILDIZ <rayyildiz@gmail.com>
2018-01-27 10:53:04 +0100

    appengine bug resolved. waiting for test.

--- internal/murmur/murmur_appengine.go
+++ internal/murmur/murmur_appengine.go
@@ -5,7 +5,7 @@ package murmur
 import "encoding/binary"
 
 func getBlock(data []byte, n int) (int64, int64) {
-	k1 := binary.LittleEndian.Int64(data[n*16:])
-	k2 := binary.LittleEndian.Int64(data[(n*16)+8:])
+	k1 := binary.LittleEndian.Uint64(data[n*16:])
+	k2 := binary.LittleEndian.Uint64(data[(n*16)+8:])
 	return k1, k2
 }
